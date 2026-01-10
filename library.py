f3d6067a698d42409855a72b33e5e64458b81105
Preethiksha-V <vpreethiksha@gmail.com>
2026-01-10 11:43:30 +0530

    Initial commit: added basic Library Management System structure

---
+++ library.py
@@ -0,0 +1,9 @@
+# Library Management System
+# Initial Version
+
+def main():
+    print("Welcome to Library Management System")
+    print("Version 1.0 - Initial Setup")
+
+if __name__ == "__main__":
+    main()
